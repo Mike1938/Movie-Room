2bb777f47af7e72a506d9d711af4156c905b4d38
Mike1938 <mmaldonadosan1938@gmail.com>
2020-10-12 15:31:53 -0700

    worked on the responsiveness of the page and adjuste styles

--- scripts/results.js
+++ scripts/results.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
     const movieSearch = new URLSearchParams(window.location.search);
-    axios.get(`http://www.omdbapi.com/?apikey=${key.apiKey}&s=${movieSearch.get("search")}`)
+    axios.get(`http://www.omdbapi.com/?apikey=${key.apiKey}&s=${movieSearch.get("search")}&type=movie`)
         .then((response) => {
             const results = response.data.Search;
-            $("h1").text(`Movie results for ${movieSearch.get("search")}`)
+            $("h1").text(`Movie results for "${movieSearch.get("search")}"`)
             console.log(results);
             $.each(results, (index, movie) => {
                 $("#movieContainer").append(
                     `<div>
-                    <h2>${movie.Title}</h2>
-                    <img src="${movie.Poster}" alt="Img not found">
+                    <h3>${movie.Title}</h3>
+                    <img class"size" src="${movie.Poster}" alt="Img not found">
+                    <p>${movie.Year}</p>
                     <form action="#" method="GET">
                     <button value="${movie.imdbID}">More Info</button>
                     </form>
