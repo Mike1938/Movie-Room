312007f4fc07bb88fc83e5d50f1684768fea521a
Mike1938 <mmaldonadosan1938@gmail.com>
2020-10-12 18:45:10 -0700

    created if stament to redirect search if movie search is null, created more styling to footer and adjusted results imgs

--- scripts/results.js
+++ scripts/results.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
     const movieSearch = new URLSearchParams(window.location.search);
-    axios.get(`http://www.omdbapi.com/?apikey=${key.apiKey}&s=${movieSearch.get("search")}&type=movie`)
-        .then((response) => {
-            const results = response.data.Search;
-            $("h1").text(`Movie results for "${movieSearch.get("search")}"`)
-            console.log(results);
-            $.each(results, (index, movie) => {
-                $("#movieContainer").append(
-                    `<div>
+    if (movieSearch.get("search") === null) {
+        window.location.replace("search.html")
+    } else {
+        axios.get(`http://www.omdbapi.com/?apikey=${key.apiKey}&s=${movieSearch.get("search")}&type=movie`)
+            .then((response) => {
+                const results = response.data.Search;
+                console.log(results)
+                $("h1").text(`Movie results for "${movieSearch.get("search")}"`)
+                $.each(results, (index, movie) => {
+                    $("#movieContainer").append(
+                        `<div>
                     <h3>${movie.Title}</h3>
                     <img class"size" src="${movie.Poster}" alt="Img not found">
                     <p>${movie.Year}</p>
@@ -15,10 +18,11 @@ $(document).ready(function () {
                     <button value="${movie.imdbID}">More Info</button>
                     </form>
                     </div>`
-                )
+                    )
+                })
             })
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 })
