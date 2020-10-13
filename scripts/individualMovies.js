b0c21c1c4b7f167c181ca55ff16c0e393eac989e
Mike1938 <mmaldonadosan1938@gmail.com>
2020-10-13 12:27:30 -0700

    Worked on the styling of the individual places and location of the info

--- scripts/individualMovies.js
+++ scripts/individualMovies.js
@@ -7,16 +7,21 @@ axios(`http://www.omdbapi.com/?apikey=${key.apiKey}&i=${movieId.get("movieId")}`
         console.log(response.data);
         const movie = response.data;
         $("#movie").append(
-            `<img id="poster" src="${movie.Poster}" alt="">
+            `<div id="sec1"><img id="poster" src="${movie.Poster}" alt=""></div>
             <div id="sec2">
-            <h1 id="title">${movie.Title} <span id="year">${movie.Year}</span></h1>
+            <h1 id="title">${movie.Title} <span id="year">(${movie.Year})</span></h1>
             <p>${movie.Plot}</p>
-            <p>Director: ${movie.Director}</p>
-            <p>Genre: ${movie.Genre}</p>
+            <p><span>Genre:</span> ${movie.Genre}</p>
+            <p><span>Runtime:</span> ${movie.Runtime}</p>
+            <p><span>Released:</span> ${movie.Released}</p>
             </div>
             <div id="sec3">
-            <p>Rated: ${movie.Rated}</p>
-            <p>Rating: ${movie.imdbRating}</p>
+            <p><span>Rated:</span> ${movie.Rated}</p>
+            <p><span>Rating:</span> ${movie.imdbRating}</p>
+            <p><span>Director:</span> ${movie.Director}</p>
+            <p><span>Writers:</span> ${movie.Writer}</p>
+            <p><span>Actors:</span> ${movie.Actors}</p>
+            <p><span>Director:</span> ${movie.Director}</p>
             </div>`
         )
     })
