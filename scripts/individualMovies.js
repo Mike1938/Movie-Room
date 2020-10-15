6375ba61f75a2b70d9219455877428175c724db7
Mike1938 <mmaldonadosan1938@gmail.com>
2020-10-15 00:10:28 -0700

    Redesigning and fixing the indvMovies pages

--- scripts/individualMovies.js
+++ scripts/individualMovies.js
@@ -1,5 +1,5 @@
 const movieId = new URLSearchParams(window.location.search);
-axios(`http://www.omdbapi.com/?apikey=${key.apiKey}&i=${movieId.get("movieId")}`)
+axios(`http://www.omdbapi.com/?apikey=${key.apiKey}&plot=full&i=${movieId.get("movieId")}`)
     .then((response) => {
         if (response.data.Response === "False") {
             window.location.replace("/pages/search.html");
@@ -7,22 +7,28 @@ axios(`http://www.omdbapi.com/?apikey=${key.apiKey}&i=${movieId.get("movieId")}`
         console.log(response.data);
         const movie = response.data;
         $("#movie").append(
-            `<div id="sec1"><img id="poster" src="${movie.Poster}" alt=""></div>
-            <div id="sec2">
-            <h1 id="title">${movie.Title} <span id="year">(${movie.Year})</span></h1>
-            <p class="info">${movie.Plot}</p>
-            <p class="info"><span>Genre:</span> ${movie.Genre}</p>
-            <p class="info"><span>Runtime:</span> ${movie.Runtime}</p>
-            <p class="info"><span>Released:</span> ${movie.Released}</p>
-            </div>
-            <div id="sec3">
-            <p class="info"><span>Rated:</span> ${movie.Rated}</p>
-            <p class="info"><span>Rating:</span> ${movie.imdbRating}</p>
-            <p class="info"><span>Director:</span> ${movie.Director}</p>
-            <p class="info"><span>Writers:</span> ${movie.Writer}</p>
-            <p class="info"><span>Actors:</span> ${movie.Actors}</p>
-            <p class="info"><span>Production:</span> ${movie.Production}</p>
-            </div>`
+            `
+                <div id="moviePoster"><img id="poster" src="${movie.Poster}" alt=""></div>
+                    <div id="shortInfo">
+                        <h1 id="title">${movie.Title}</h1>
+                    <ul>
+                        <li class="info"><span>Genre:</span> ${movie.Genre}</li>
+                        <li class="info"><span>Runtime:</span> ${movie.Runtime}</li>
+                        <li class="info"><span>Released:</span> ${movie.Released}</li>
+                        <li class="info"><span>Rated:</span> ${movie.Rated}</li>
+                        <li class="info"><span>Rating:</span> ${movie.imdbRating}/10</li>
+                        <li class="info"><span>Awards:</span> ${movie.Awards}</li>
+                    </ul>
+                </div>
+                <div id="lgInfo">
+                <h2>plot</h2>
+                    <p class="info">${movie.Plot}</p>
+                    <p class="info"><span>Director:</span> ${movie.Director}</p>
+                    <p class="info"><span>Writers:</span> ${movie.Writer}</p>
+                    <p class="info"><span>Actors:</span> ${movie.Actors}</p>
+                    <p class="info"><span>Production:</span> ${movie.Production}</p>
+                </div>
+            `
         )
     })
     .catch((error) => {
