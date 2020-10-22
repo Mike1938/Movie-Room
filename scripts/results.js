0e78533edceba98a7b694625f82445bc510d2228
Mike1938 <mmaldonadosan1938@gmail.com>
2020-10-21 23:45:37 -0700

    Added another movie in feature films and modified the grid sytem to fit the movie also resize the form in the first section in index.html, also added hr in between sections. Did an if statement in results.js to write movie not found when results is undefined

--- scripts/results.js
+++ scripts/results.js
@@ -1,32 +1,35 @@
 $(document).ready(function () {
     const movieSearch = new URLSearchParams(window.location.search);
     if (movieSearch.get("search") === null) {
-        window.location.replace("search.html")
+        window.location.replace("search.html");
     } else {
         axios.get(`http://www.omdbapi.com/?apikey=${key.apiKey}&s=${movieSearch.get("search")}&type=movie`)
             .then((response) => {
+                $("h1").text(`Movie results for "${movieSearch.get("search")}"`);
                 const results = response.data.Search;
-                console.log(results)
-                $("h1").text(`Movie results for "${movieSearch.get("search")}"`)
-                $.each(results, (index, movie) => {
-                    let movieTitle = movie.Title;
-                    if (movieTitle.length > 35) {
-                        movieTitle = `${movie.Title.slice(0, 40)} ...`;
-                    }
-                    $("#movieContainer").append(
-                        `<div>
-                    <h3>${movieTitle}</h3>
-                    <img class"size" src="${movie.Poster}" alt="Img not found">
-                    <p>${movie.Year}</p>
-                    <form action="/pages/individualMovies.html" method="GET">
-                    <button name="movieId" value="${movie.imdbID}">More Info</button>
-                    </form>
-                    </div>`
-                    )
-                })
+                if (results === undefined) {
+                    $("#movieContainer").append(`<h1 id="wasNotFound">The Movie "${movieSearch.get("search")}" was not found, please try again...</h1>`);
+                } else {
+                    $.each(results, (index, movie) => {
+                        let movieTitle = movie.Title;
+                        if (movieTitle.length > 35) {
+                            movieTitle = `${movie.Title.slice(0, 40)} ...`;
+                        }
+                        $("#movieContainer").append(
+                            `<div>
+                            <h3>${movieTitle}</h3>
+                            <img class"size" src="${movie.Poster}" alt="Img not found">
+                            <p>${movie.Year}</p>
+                            <form action="/pages/individualMovies.html" method="GET">
+                            <button name="movieId" value="${movie.imdbID}">More Info</button>
+                            </form>
+                        </div>`
+                        )
+                    });
+                }
             })
             .catch((error) => {
                 console.log(error);
-            })
+            });
     }
-})
+});
