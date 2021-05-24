8d8fdb6e68203375986bb4a16ba9c855849aec82
Mike1938 <mmaldonadosan1938@gmail.com>
2021-05-23 22:54:31 -0500

    joined individualMovies.css with styles.css. adjusted and added classes in span in individualMovies.js adjusted id and classes in individualMovies.html and about.html

--- scripts/individualMovies.js
+++ scripts/individualMovies.js
@@ -15,21 +15,21 @@ axios(`http://www.omdbapi.com/?apikey=${key.apiKey}&plot=full&i=${movieId.get("m
                     <div id="shortInfo">
                         <h1 id="title">${movie.Title}</h1>
                     <ul>
-                        <li class="info"><span>Genre:</span> ${movie.Genre}</li>
-                        <li class="info"><span>Runtime:</span> ${movie.Runtime}</li>
-                        <li class="info"><span>Released:</span> ${movie.Released}</li>
-                        <li class="info"><span>Rated:</span> ${movie.Rated}</li>
-                        <li class="info"><span>Rating:</span> <span id="ratingStar"><i class="fas fa-star fa-xs"></i></span> ${movie.imdbRating}/10</li>
-                        <li class="info"><span>Awards:</span> ${movie.Awards}</li>
+                        <li class="info"><span class = 'infoLabel'>Genre:</span> ${movie.Genre}</li>
+                        <li class="info"><span class = 'infoLabel'>Runtime:</span> ${movie.Runtime}</li>
+                        <li class="info"><span class = 'infoLabel'>Released:</span> ${movie.Released}</li>
+                        <li class="info"><span class = 'infoLabel'>Rated:</span> ${movie.Rated}</li>
+                        <li class="info"><span class = 'infoLabel'>Rating:</span> <span id="ratingStar"><i class="fas fa-star fa-xs"></i></span> ${movie.imdbRating}/10</li>
+                        <li class="info"><span class = 'infoLabel'>Awards:</span> ${movie.Awards}</li>
                     </ul>
                 </div>
                 <div id="lgInfo">
                 <h2>plot</h2>
                     <p class="info">${movie.Plot}</p>
-                    <p class="info"><span>Director:</span> ${movie.Director}</p>
-                    <p class="info"><span>Writers:</span> ${movie.Writer}</p>
-                    <p class="info"><span>Actors:</span> ${movie.Actors}</p>
-                    <p class="info"><span>Production:</span> ${movie.Production}</p>
+                    <p class="info"><span class = 'infoLabel'>Director:</span> ${movie.Director}</p>
+                    <p class="info"><span class = 'infoLabel'>Writers:</span> ${movie.Writer}</p>
+                    <p class="info"><span class = 'infoLabel'>Actors:</span> ${movie.Actors}</p>
+                    <p class="info"><span class = 'infoLabel'>Production:</span> ${movie.Production}</p>
                 </div>
             `
         )
