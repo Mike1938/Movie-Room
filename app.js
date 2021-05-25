f41cf0b43d582e4fbb2ddbf728bbcc9b68543c52
Mike1938 <mmaldonadosan1938@gmail.com>
2021-05-25 13:22:44 -0500

    Started working the logic to request movies info from api, added ejs template in results.ejs

--- app.js
+++ app.js
@@ -17,9 +17,28 @@ app.get('/about', (req, res) => {
 app.get('/legal', (req, res) => {
     res.render('legal');
 })
+
 app.get('/search', (req, res) => {
-    res.render('search')
+    const { q } = req.query;
+    if (!q) {
+        res.render('search');
+    }
+    else {
+        axios.get(`http://www.omdbapi.com/?apikey=${process.env.apikey}&s=${q}&type=movie`)
+            .then((response) => {
+                const results = response.data.Search;
+                if (response.data.Response === 'False') {
+                    res.send('Theres an error');
+                } else {
+                    res.render('results', { data: results, search: q })
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+                res.render('Theres an error')
+            })
+    }
 })
 app.listen(3000, () => {
-    console.log('Server running at port 3000');
+    console.log("Server running on port 3000");
 })
