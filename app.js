0d394f27e059bc739b67041fca776f12033970a3
Mike1938 <mmaldonadosan1938@gmail.com>
2021-05-26 15:53:29 -0500

    Created function to check if theres pictures from the api request, if not then notfound pic is added. created function to verify the length of Title so that it doesn't affect the style of the page because of the length.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const axios = require('axios');
 const ejs = require('ejs');
+const { response } = require('express');
 
 app.set("view engine", 'ejs');
 app.use(express.static('./public'));
@@ -17,19 +18,39 @@ app.get('/about', (req, res) => {
 app.get('/legal', (req, res) => {
     res.render('legal');
 })
-
+const verifyImg = data => {
+    for (let i = 0; i < data.length; i++) {
+        let poster = data[i].Poster;
+        if (poster === 'N/A') {
+            poster = "/images/notFound.jpg"
+            data[i].Poster = poster
+        }
+    }
+    return data;
+}
+const verifyStrLen = data => {
+    for (let str of data) {
+        let title = str.Title;
+        if (title.length > 35) {
+            str.Title = `${title.slice(0, 35)}...`;
+        }
+    }
+    return data;
+}
 app.get('/search', (req, res) => {
     const { q } = req.query;
     if (!q) {
-        res.render('search', {error: "No movie was typed, Try Again"});
+        res.render('search', { error: "No movie was typed, Try Again" });
     }
     else {
         axios.get(`http://www.omdbapi.com/?apikey=${process.env.apikey}&s=${q}&type=movie`)
             .then((response) => {
                 const results = response.data.Search;
                 if (response.data.Response === 'False') {
-                    res.render('results', {data: results, search: q});
+                    res.render('results', { data: results, search: q });
                 } else {
+                    verifyImg(results);
+                    verifyStrLen(results);
                     res.render('results', { data: results, search: q })
                 }
             })
