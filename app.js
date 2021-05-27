b77e75c69c2e8e045c8fcaf71b81d894cb4840af
Mike1938 <mmaldonadosan1938@gmail.com>
2021-05-26 21:05:37 -0400

    Worked on the individualMovies page so that we can view more details about the movies. Also created a async function of fetching movies that way i dont repeat the fetch code

--- app.js
+++ app.js
@@ -37,29 +37,48 @@ const verifyStrLen = data => {
     }
     return data;
 }
-app.get('/search', (req, res) => {
+const fetchMovies = async (search , type) =>{
+    let searchMethod;
+    if(type === 'id'){
+        searchMethod = `&i=${search}`
+    }else{
+        searchMethod = `&s=${search}&type=movie`
+    }
+    try{
+        const results = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.apikey}${searchMethod}`)
+        return results.data
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+app.get('/search', async(req, res) => {
     const { q } = req.query;
     if (!q) {
         res.render('search', { error: "No movie was typed, Try Again" });
     }
     else {
-        axios.get(`http://www.omdbapi.com/?apikey=${process.env.apikey}&s=${q}&type=movie`)
-            .then((response) => {
-                const results = response.data.Search;
-                if (response.data.Response === 'False') {
-                    res.render('results', { data: results, search: q });
-                } else {
-                    verifyImg(results);
-                    verifyStrLen(results);
-                    res.render('results', { data: results, search: q })
-                }
-            })
-            .catch((e) => {
-                console.log(e);
-                res.render('Theres an error');
-            })
+            const results = await fetchMovies(q, 'search');
+            if (results.Response === 'False') {
+                res.render('results', { data: results.Response, search: q });
+            } else {
+                verifyImg(results.Search);
+                verifyStrLen(results.Search);
+                res.render('results', { data: results.Search, search: q })
+            }
     }
 })
+app.get('/search/movieDetails/:movId', async (req, res)=>{
+    const {movId} = req.params;
+    const results = await fetchMovies(movId, 'id');
+    if (results.Response === 'False') {
+        res.render('individualMovies', { data: results.Response, search: movId });
+    } else {
+        verifyImg(results);
+        res.render('individualMovies', { data: results})
+    }
+})
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
